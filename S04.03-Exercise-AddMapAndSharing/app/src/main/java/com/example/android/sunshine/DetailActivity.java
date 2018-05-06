1c9a1a833339b234c85add84db07914774b98953
Ramsha Saeed <ramshasaeed2016@gmail.com>
2018-05-07 04:44:54 +0500

    Completed S04.03-Solution-AddMapAndSharing

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -34,8 +34,8 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (3) Create a menu with an item with id of action_share
-    // TODO (4) Display the menu and implement the forecast sharing functionality
+    // Completed (3) Create a menu with an item with id of action_share
+    // Completed (4) Display the menu and implement the forecast sharing functionality
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
